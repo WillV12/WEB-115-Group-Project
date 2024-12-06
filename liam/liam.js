71157d66157a98e7e02ef12649890095436e1079
lgift <lpgift@my.waketech.edu>
2024-12-06 10:11:32 -0500

    EPIC SAUCE DAY 1
    im gonnw ablow

---
+++ liam/liam.js
@@ -0,0 +1,7 @@
+let column1 = parseInt((Math.random() * 5) + 1)
+let column2 = parseInt((Math.random() * 5) + 1)
+let column3 = parseInt((Math.random() * 5) + 1)
+
+console.log(column1)
+console.log(column2)
+console.log(column3)
